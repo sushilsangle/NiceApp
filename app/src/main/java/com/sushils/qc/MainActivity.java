51b232d85308413b47163eb3f2ce982d6c04f188
sushil <sushilsangle.ss@gmail.com>
2019-02-13 17:19:28 +0530

    I have design Map and status button Successfully in Driver app.

--- app/src/main/java/com/sushils/qc/MainActivity.java
+++ app/src/main/java/com/sushils/qc/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -21,6 +21,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.sushils.qc.model.User;
+
+import dmax.dialog.SpotsDialog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -102,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
 
                 dialogInterface.dismiss();
 
+
+                //Set disbale button Sign in if is processing.
+
+                //btnSignIn.isEnabled(false);
+
                 //TODO check validation..
                 if (TextUtils.isEmpty(edtMobile.getText().toString())) {
                     Snackbar.make(rootLaout, "Please enter mobile number", Snackbar.LENGTH_SHORT).show();
@@ -117,19 +124,31 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                final AlertDialog waitingDialog  = new SpotsDialog(MainActivity.this);
+                waitingDialog.show();
+
+
                 //TODO Login..
 
                 mAuth.signInWithEmailAndPassword(edtMobile.getText().toString(),edtPassword.getText().toString())
                         .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
+
+                                waitingDialog.dismiss();
+
                                   startActivity(new Intent(MainActivity.this,Welcome.class));
                                   finish();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        waitingDialog.dismiss();
                         Snackbar.make(rootLaout,"Failed "+e.getMessage(),Snackbar.LENGTH_SHORT).show();
+
+                        //TODO Active signIn button
+
+                        //btnSignIn.isEnabled(true);
                     }
                 });
             }
